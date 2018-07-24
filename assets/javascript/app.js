e997acf7a6542a49f158cb7ef4b3b1c54688c37d
Timothy Bedsole <timbedsole@gmail.com>
2018-07-24 12:55:27 -0400

    added styling for start game

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,19 +1,24 @@
-// api url
-//https://opentdb.com/api.php?amount=10
-
-
 //Global variables
 var correctAnswers = 0;
 var incorrectAnswers = 0;
 var timeoutAnswers = 0;
 var questionCount = 0;
+var gameIsRunning = false;
 
 
 //API call
-$.getJSON("https://opentdb.com/api.php?amount=10&category=9&type=multiple", function(data){
+$.getJSON("https://opentdb.com/api.php?amount=10&category=29&type=multiple", function(data){
     // console.log(data);
     console.log(data.results);
     console.log(data.results[0].question)
 
     $(".question").text(data.results[0].question);
+})
+
+window.onload = function(){
+    // $("#triviaContainer").hide();
+}
+
+$("#startGameContainer").click(function(){
+    alert("Start game clicked");
 })
