62f04d28b63ec04474a9e8b3c8b04b8242f8c0f2
Timothy Bedsole <timbedsole@gmail.com>
2018-07-25 18:11:41 -0400

    fixed issue where timer was counting down before game started

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -20,23 +20,28 @@ var countDown = function(){
 //Function that stops the countdown timer
 var stopTimer = function(){
     clearInterval(myTimer);
-    time = 30;
 }
 
 //Function that calls countDown() and runs it every second
-var myTimer = setInterval(function(){
-    if (time >= 0){
-        countDown();
-    }
-}, 1000)
+var myTimer;
 
 
 //API call
-$.getJSON("https://opentdb.com/api.php?amount=10&category=29&type=multiple", function(data){    
-    //Function that assigns each possible answer to a random button for each question
+$.getJSON("https://opentdb.com/api.php?amount=10&category=29&type=multiple", function(data){
+    
     var renderQuestion = function(){
+        
+        //starts count down timer for the question
+        myTimer = setInterval(function(){
+            if (questionCount < 10 && time >= 0){
+                countDown();
+            }
+        }, 1000)
+        
         //Puts the question at the current index in the Div displayed to the user
         $(".question").text(data.results[questionCount].question);
+
+        //Function that assigns each possible answer to a random button for each question
         data.results[questionCount].incorrect_answers.forEach(function(value, index){
             while (buttonArr.length <= (index + 1)){
                 if (buttonArr.indexOf(buttonNum) === -1){
@@ -48,17 +53,17 @@ $.getJSON("https://opentdb.com/api.php?amount=10&category=29&type=multiple", fun
             $("#button" + buttonNum).text(value);
             $("#button" + buttonNum).attr("value", "incorrect");
         });
+
         //Puts the correct answer in the one remaining empty button
         while (buttonArr.indexOf(buttonNum) > 0){
             randomNum();
         };
-        // console.log("This number should NOT be in the array logged above: " + buttonNum);
         $("#button" + buttonNum).text(data.results[questionCount].correct_answer);
         $("#button" + buttonNum).attr("value", "correct");
         buttonArr = [];
     };
 
-    //Start button event listener that hides the start button and shows the trivia questions
+    //Start button event listener. Once clicked, hides the start button and shows the trivia questions
     $("#startGameContainer").click(function(){
         $("#startGameContainer").hide();
         $("#triviaContainer").show();
@@ -67,7 +72,7 @@ $.getJSON("https://opentdb.com/api.php?amount=10&category=29&type=multiple", fun
 
     //Event listener for clicks on the answer buttons
     $("button").click(function(){
-        //If the button is the correct answer, the correct answer count is increased, question count is increased, and the next question is rendered
+        //If the button is the correct answer, the correct answer count is increased and the answer screen is shown
         if ($(this).attr("value") == "correct"){
             console.log("clicked correct button");
             time = 30;
@@ -75,7 +80,7 @@ $.getJSON("https://opentdb.com/api.php?amount=10&category=29&type=multiple", fun
             answerIsCorrect = true;
             revealAnswer();
         }
-        //If the button clicked is the incorrect answer, incorrect answer counter is increased, question count is increased, and the next question is rendered
+        //If the button clicked is the incorrect answer, incorrect answer counter is increased and the answer screen is shown
         else if ($(this).attr("value") == "incorrect"){
             console.log("clicked incorrect button");
             time = 30;
@@ -84,25 +89,27 @@ $.getJSON("https://opentdb.com/api.php?amount=10&category=29&type=multiple", fun
             revealAnswer();
         }
     });
+
+    //Function that tells the user if they got the answer right or wrong
     var revealAnswer = function(){
         stopTimer();
+        //Set time out goes here
         $("#triviaContainer").hide();
         if (answerIsCorrect){
             $("#result").html("<h3>Correct!</h3>")
-            $("#answerImg").html("<img src='assets/images/deadpoolThumbsUp.jpg'>") //IMAGE FOR CORRECT GUESS WILL GO HERE
+            $("#answerImg").html("<img src='assets/images/deadpoolThumbsUp.jpg'>")
         } else if (!answerIsCorrect){
             $("#result").html("<h3>Nope!</h3>")
             $("#correctAnswer").text("The correct answer is " + data.results[questionCount].correct_answer);
-            $("#answerImg").html("<img src='assets/images/deadpoolThumbsDown.gif'>") //IMAGE FOR INCORRECT GUESS WILL GO HERE
+            $("#answerImg").html("<img src='assets/images/deadpoolThumbsDown.gif'>")
         } else {
             $("#result").text("Times up!")
             $("#correctAnswer").text("The correct answer is " + data.results[questionCount].correct_answer);
-            $("#answerImg").html("<img src='assets/images/deadpoolThumbsDown.gif'>") //IMAGE FOR INCORRECT GUESS WILL GO HERE
+            $("#answerImg").html("<img src='assets/images/deadpoolThumbsDown.gif'>")
         }
         $("#answer").show();
         questionCount++;
         gameOverCheck();
-        // renderQuestion();
     };
 })
 
@@ -118,14 +125,15 @@ var gameOverCheck = function(){
     }
 }
 
-//Hides the questions and score screen on load
+//Hides the questions,score and answer screen on load
 $(document).ready(function(){
     $("#triviaContainer").hide();
     $("#finalScoreContainer").hide();
     $("#answer").hide();
     
+    //Listens for click on the play again button at the final score screen
+    //If clicked, the game restarts
     $("#playAgain").click(function(){
-        console.log("Play again clicked")
         questionCount = 0;
         correctAnswers = 0;
         incorrectAnswers = 0;
